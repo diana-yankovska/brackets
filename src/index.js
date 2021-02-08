a57f11c732cec669ddd4f5a645dd86c0bcb87bb0
Diana Yankovskaya <dianashpandaruk@mail.ru>
2021-02-08 16:21:16 +0300

    feat: implement check brackets correctness function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arrForCheck = str.split("");
+
+  if (arrForCheck.length % 2 != 0) {
+    return false;
+  }
+
+  let stackArr = [];
+  arrForCheck.forEach(element => {
+    for (let i = 0; i < bracketsConfig.length; i++) {
+      let startBracket = bracketsConfig[i][0];
+      let closingBracket = bracketsConfig[i][1];
+
+      switch (element) {
+        case startBracket:
+          if (startBracket === closingBracket && stackArr[stackArr.length - 1] === startBracket) {
+            stackArr.pop();
+            break;
+          } else {
+            stackArr.push(element);
+            break;
+          }
+        case closingBracket:
+          if (stackArr[stackArr.length - 1] === startBracket) {
+            stackArr.pop();
+            break;
+          } else {
+            return false;
+          }
+      }
+    }
+  });
+  return stackArr.length === 0;
 }
